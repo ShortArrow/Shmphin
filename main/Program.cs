36245ccea4c4c7f90b604a21c92b503b3ea3809d
ShortArrow <bamboogeneral@shortarrow.jp>
2024-10-06 22:36:13 +0900

    Refactor Program.cs to use Spectre.Console for improved layout and styling

--- main/Program.cs
+++ main/Program.cs
@@ -4,7 +4,7 @@ namespace main;
 
 class Program
 {
-  static void Main(string[] args)
+  static Layout CreateLayout()
   {
     // Create the layout
     var layout = new Layout("Root")
@@ -22,7 +22,35 @@ class Program
                 new Markup("Hello [blue]World![/]"),
                 VerticalAlignment.Middle))
             .Expand());
+    return layout;
+  }
+  static void Main(string[] args)
+  {
+    string input = string.Empty;
+    AnsiConsole.Clear();
+    AnsiConsole.Live(new Markup("[bold green]Start Shmphin.[/]"))
+    .Start(ctx =>
+    {
+      int counter = 0;
+      var inputTask = new Thread(() =>
+      {
+        while (input != "q")
+        {
+          var key = Console.ReadKey(true);
+          input = key.KeyChar.ToString().ToLower();
 
-    AnsiConsole.Write(layout);
+        }
+      });
+      inputTask.Start();
+      while (input != "q")
+      {
+        ctx.UpdateTarget(CreateLayout());
+        Thread.Sleep(1000);
+        counter++;
+      }
+      inputTask.Join();
+    });
+    AnsiConsole.Clear();
+    AnsiConsole.MarkupLine("[bold red]Shmphin is Finished.[/]");
   }
 }
