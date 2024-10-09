2a2ebdc700e5f36a5d73e63698049321449bf62b
ShortArrow <bamboogeneral@shortarrow.jp>
2024-10-09 19:47:45 +0900

    Refactor layout.cs to update UI layout and improve panel alignment

--- main/ui/layout.cs
+++ main/ui/layout.cs
@@ -24,22 +24,34 @@ class Ui
 
     // Update the left column
     layout["Header"].Update(
-        new Panel(Align.Center(new Markup($"[blue]{memory.Params.SharedMemoryName}[/]"),
-                               VerticalAlignment.Middle))
-            .Expand());
+      new Panel(Align.Center(
+        new Markup($"[blue]{memory.Params.SharedMemoryName}[/]"),
+        VerticalAlignment.Middle
+      ))
+      .Expand());
     layout["Main"]["Right"].Update(
-        new Panel(Align.Center(new Markup($"[green]{message}[/]"),
-                               VerticalAlignment.Middle))
-            .Expand());
+      new Panel(Align.Center(
+        new Markup($"[green]{message}[/]"),
+        VerticalAlignment.Middle
+      ))
+      .Expand()
+    );
     layout["Main"]["Left"].Update(
-        new Panel(Align.Center(new Markup($"[yellow]{BitConverter.ToString(memory.SnapShot.Current)}[/]"),
-                               VerticalAlignment.Middle)).BorderColor(IsExMode ? Color.Default: Color.Green)
-            .Expand());
+      new Panel(Align.Center(
+        new Markup($"[yellow]{BitConverter.ToString(memory.SnapShot.Current)}[/]"),
+        VerticalAlignment.Middle
+      ))
+      .BorderColor(IsExMode ? Color.Default : Color.Green)
+      .Expand()
+    );
     layout["Footer"].Update(
-        new Panel(Align.Center(new Markup($"[green]{Input.inputBuffer}[/]"),
-                               VerticalAlignment.Middle))
-            .BorderColor(IsExMode ? Color.Green: Color.Default)
-            .Expand());
+      new Panel(Align.Center(
+        new Markup($"[green]{Input.inputBuffer}[/]"),
+        VerticalAlignment.Middle
+      ))
+      .BorderColor(IsExMode ? Color.Green : Color.Default)
+      .Expand()
+    );
     return layout;
   }
   private void CreateDiffView()
