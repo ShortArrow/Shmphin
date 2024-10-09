5f6013be1af5f21aa63fbfe2b98174e6f3560086
ShortArrow <bamboogeneral@shortarrow.jp>
2024-10-09 15:27:26 +0900

    Refactor layout.cs to update UI layout and add footer panel

--- main/ui/layout.cs
+++ main/ui/layout.cs
@@ -8,21 +8,29 @@ class Ui
   public static Layout CreateLayout(string message)
   {
     // Create the layout
-    var layout = new Layout("Root").SplitColumns(
+    var layout = new Layout("Root").SplitRows(
+      new Layout("Header").Size(3),
+      new Layout("Main").SplitColumns(
         new Layout("Left"),
-        new Layout("Right").SplitRows(new Layout("Top"), new Layout("Bottom")));
+        new Layout("Right")),
+      new Layout("Footer").Size(3)
+    );
 
     // Update the left column
-    layout["Left"].Update(
+    layout["Header"].Update(
         new Panel(Align.Center(new Markup($"[blue]{memory.Params.SharedMemoryName}[/]"),
                                VerticalAlignment.Middle))
             .Expand());
-    layout["Right"]["Top"].Update(
+    layout["Main"]["Right"].Update(
         new Panel(Align.Center(new Markup($"[green]{message}[/]"),
                                VerticalAlignment.Middle))
             .Expand());
-    layout["Right"]["Bottom"].Update(
+    layout["Main"]["Left"].Update(
         new Panel(Align.Center(new Markup($"[yellow]{BitConverter.ToString(memory.SnapShot.Current)}[/]"),
+                               VerticalAlignment.Middle))
+            .Expand());
+    layout["Footer"].Update(
+        new Panel(Align.Center(new Markup($"[green]Shmphin[/]"),
                                VerticalAlignment.Middle))
             .Expand());
     return layout;
