accb170561864e1bf67d4bdab7bd23e30c221784
ShortArrow <bamboogeneral@shortarrow.jp>
2024-10-13 01:56:45 +0900

    Refactor keymap.cs to add a dictionary for key mappings

---
+++ main/ui/keymap.cs
@@ -0,0 +1,17 @@
+class KeyMap
+{
+  static readonly Dictionary<string, string> keyMap = new(
+    [
+      new("u", "update display with read from memory"),
+      new(":", "command mode"),
+      new("q", "quit"),
+      new("h", "help"),
+      new("j", "move cursor down"),
+      new("k", "move cursor up"),
+      new("l", "move cursor right"),
+      new("h", "move cursor left"),
+      new("m", "change grid type"),
+      new("c", "change memory with input")
+    ]
+  );
+}
