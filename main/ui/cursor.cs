b5bdb79b13a95af1217a2d6b9b8f7d3366e17b23
ShortArrow <bamboogeneral@shortarrow.jp>
2024-10-22 07:56:28 +0900

    Refactor Cursor class to handle boundary conditions when moving up, down, left, and right

--- main/ui/cursor.cs
+++ main/ui/cursor.cs
@@ -20,21 +20,25 @@ class Cursor
   {
     Debug.WriteLine($"move up: x: {x}, y: {y}");
     if (y > 0) { y--; }
+    else { y = 0; }
   }
   public static void MoveDown()
   {
     Debug.WriteLine($"move down: x: {x}, y: {y}");
     var height = memory.Params.Size / (GridMode.ColumnsLength * GridMode.CellLength);
     if (y < height - 1) { y++; }
+    else { y = height - 1; }
   }
   public static void MoveLeft()
   {
     Debug.WriteLine($"move left: x: {x}, y: {y}");
     if (x > 0) { x--; }
+    else { x = 0; }
   }
   public static void MoveRight()
   {
     Debug.WriteLine($"move right: x: {x}, y: {y}");
     if (X < GridMode.ColumnsLength - 1) { x++; }
+    else { x = GridMode.ColumnsLength - 1; }
   }
 }
