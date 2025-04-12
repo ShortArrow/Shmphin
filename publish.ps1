1d735b2aa1915389b82215620054de60720672dd
ShortArrow <bamboogeneral@shortarrow.jp>
2025-04-13 06:55:52 +0900

    Add README.md and enhance publish script for multi-platform support

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,17 @@
 #! pwsh
 
-dotnet publish main -c Release
+$targets = @{
+  "win-x64" = "Shmphin.exe"
+  "linux-x64" = "Shmphin"
+  "linux-arm64" = "Shmphin"
+  "linux-arm" = "Shmphin"
+}
 
-$name = "Shmphin.exe"
+foreach ($key in $targets.Keys) {
+  dotnet publish -c Release main -r $key
+  mkdir -Force "./release/$key"
+  Copy-Item "./main/bin/Release/net8.0/$key/publish/$($targets[$key])" "./release/$key/"
+  Get-ChildItem "./release/$key/$($targets[$key])"
+  Write-Host "Copied $($targets[$key]) to ./release/$key/"
+}
 
-mkdir -Force "./release/"
-Copy-Item "./main/bin/Release/net8.0/publish/$name" "./release/"
-Get-ChildItem "./release/$name"
